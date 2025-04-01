47a787f6ed7ae4dee70ec8ebd26fcfcec126310a
Lucas SA <lusanchesx@gmail.com>
2025-04-01 12:43:01 -0300

    redirecionando rotas nao encontradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import ProductDetails from './routes/ClientHome/ProductDetails'
 import ClientHome from './routes/ClientHome'
 import Catalog from './routes/ClientHome/Catalog'
@@ -12,6 +12,7 @@ function App() {
           <Route path='catalog' element={<Catalog/>}></Route>
           <Route path='product-details/:producId' element={<ProductDetails/>}></Route>
         </Route>
+        <Route path='*' element={<Navigate to="/"/>}></Route>
       </Routes>
     </BrowserRouter>
   )
